0f107f43273dacfa4d1a30abec9fadc7518cee5b
teresating886 <teresating886@gmail.com>
2018-12-22 09:56:00 -0700

    Update index.js

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,6 +1,7 @@
 const canvas = d3.select(".canvas");
 const svg = canvas.append ('svg');
-
+svg.attr('height', 600);
+svg.attr('weight', 600);
 // append shapes to svg container
 svg.append('rect');
 svg.append('circle');
