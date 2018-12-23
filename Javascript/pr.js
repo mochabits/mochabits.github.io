f22863bc874b0392aea2e18692f0ef724a4f2719
teresating886 <teresating886@gmail.com>
2018-12-23 15:18:32 -0700

    Update pr.js

--- Javascript/pr.js
+++ Javascript/pr.js
@@ -1,9 +1,10 @@
 const data = [
   {width:200, height:100; fill:"purple"}
-]
+];
 
 const svg = d3.select("svg");
 svg.select("rect")
-  .attr("width",200)
-  .attr("height",100)
-  .attr("fill","pink")
+  .data()
+  .attr("width",function(d){return d.width})
+  .attr("height",function(d){return d.height})
+  .attr("fill",function(d){return d.fill})
