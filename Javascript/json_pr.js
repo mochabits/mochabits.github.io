237a9e3e7ee92033518cd046c5b5afd7ca311a17
teresating886 <teresating886@gmail.com>
2018-12-23 20:02:22 -0700

    json

---
+++ Javascript/json_pr.js
@@ -0,0 +1,18 @@
+// select svg container
+const svg = d3.select("svg");
+d3.json("planets.json").then(data => {
+  const cirs = svg.selectAll("circle")
+  .data(data);
+  // add attr to the circle already exist
+  cirs.attr("cy", 200)
+      .attr("cx", d=>d.distance)
+      .attr("r", d=>d.radius)
+      .attr("fill", d=>d.fill);
+  // enter data and new circles
+  cirs.enter()
+      .append(circle)
+      .attr("cy", 200)
+      .attr("cx", d=>d.distance)
+      .attr("r", d=>d.radius)
+      .attr("fill", d=>d.fill);
+})
