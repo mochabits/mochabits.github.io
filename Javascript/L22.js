cb5d6e6b8226a7846cf0073b472adf121fe1e91d
teresating886 <teresating886@gmail.com>
2018-12-24 10:44:22 -0700

    Update L22.js

--- Javascript/L22.js
+++ Javascript/L22.js
@@ -1,18 +1,28 @@
 // select svg container
 const svg = d3.select("svg");
 d3.json("menu.json").then(data => {
+
+const y = d3.scaleLinear()
+            .domain([0,1000])
+            .range([0,500]);
+
+// testing if it works
+// console.log(y(400));
+// console.log(y(0));
+// console.log(y(800));
+
   // join the data to rects
   const rects = svg.selectAll("rect")
                    .data(data)
         rects.attr("width", 50)
-             .attr("height",d => d.orders)
+             .attr("height",d => y(d.orders) )
              .attr("fill","orange")
              .attr("x", (d,i) => i*70);
   // append the rest of rects
   rects.enter()
        .append("rect")
        .attr("width", 50)
-      .attr("height",d => d.orders)
+      .attr("height",d => y(d.orders) )
       .attr("fill","orange")
       .attr("x", (d,i) => i*70);
 
