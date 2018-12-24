050c36d5fe214d16734134140fa75f0be2cd5a40
teresating886 <teresating886@gmail.com>
2018-12-23 20:46:51 -0700

    changes

--- Javascript/L22.js
+++ Javascript/L22.js
@@ -1,2 +1,12 @@
 // select svg container
 const svg = d3.select("svg");
+d3.json("menu.json").then(data => {
+  // join the data to rects
+  const rects = svg.selectAll("rect")
+                   .data(data)
+        rects.attr("width", 50)
+             .attr("height",d => d.orders)
+             .attr("fill","orange")
+             .attr("x", (d,i) => i*70)
+
+})
