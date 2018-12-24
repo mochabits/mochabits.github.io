627de67eaf8e055d7411dd74006069c88e553baa
teresating886 <teresating886@gmail.com>
2018-12-23 21:08:46 -0700

    Update L22.js

--- Javascript/L22.js
+++ Javascript/L22.js
@@ -7,6 +7,13 @@ d3.json("menu.json").then(data => {
         rects.attr("width", 50)
              .attr("height",d => d.orders)
              .attr("fill","orange")
-             .attr("x", (d,i) => i*70)
+             .attr("x", (d,i) => i*70);
+  // append the rest of rects
+  rects.enter()
+       .append("rect")
+       .attr("width", 50)
+      .attr("height",d => d.orders)
+      .attr("fill","orange")
+      .attr("x", (d,i) => i*70);
 
 })
