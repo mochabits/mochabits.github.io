0d3d95909b0e8767bbb7f2608f01e5e896a4a89b
teresating886 <teresating886@gmail.com>
2018-12-24 11:01:39 -0700

    Update L22.js

--- Javascript/L22.js
+++ Javascript/L22.js
@@ -18,14 +18,14 @@ const x = d3.scaleBand()
   // join the data to rects
   const rects = svg.selectAll("rect")
                    .data(data)
-        rects.attr("width", 50)
+        rects.attr("width", x.bandwidth)
              .attr("height",d => y(d.orders) )
              .attr("fill","orange")
              .attr("x", (d,i) => i*70);
   // append the rest of rects
   rects.enter()
        .append("rect")
-       .attr("width", 50)
+       .attr("width", x.bandwidth)
       .attr("height",d => y(d.orders) )
       .attr("fill","orange")
       .attr("x", (d,i) => i*70);
