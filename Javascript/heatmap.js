08129d9b489754f84a4170df88160cd9c96439e0
teresating886 <teresating886@gmail.com>
2019-01-05 10:43:09 -0700

    Update heatmap.js

--- Javascript/heatmap.js
+++ Javascript/heatmap.js
@@ -1,3 +1,6 @@
+
+
+// chart settings
 var margin = { top: 50, right: 0, bottom: 100, left: 30 },
 width = 960 - margin.left - margin.right,
 height = 430 - margin.top - margin.bottom,
@@ -5,6 +8,7 @@ gridSize = Math.floor(width / 24),
 legendElementWidth = gridSize*2,
 buckets = 9,
 colors = ["#ffffd9","#edf8b1","#c7e9b4","#7fcdbb","#41b6c4","#1d91c0","#225ea8","#253494","#081d58"], // alternatively colorbrewer.YlGnBu[9]
+
 days = ["Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"],
 times = ["1a", "2a", "3a", "4a", "5a", "6a", "7a", "8a", "9a", "10a", "11a", "12a", "1p", "2p", "3p", "4p", "5p", "6p", "7p", "8p", "9p", "10p", "11p", "12p"];
 datasets = ["data.tsv", "data2.tsv"];
@@ -15,26 +19,33 @@ var svg = d3.select("#chart").append("svg")
 .append("g")
 .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-var dayLabels = svg.selectAll(".dayLabel")
-.data(days)
-.enter().append("text")
-  .text(function (d) { return d; })
-  .attr("x", 0)
-  .attr("y", function (d, i) { return i * gridSize; })
-  .style("text-anchor", "end")
-  .attr("transform", "translate(-6," + gridSize / 1.5 + ")")
-  .attr("class", function (d, i) { return ((i >= 0 && i <= 4) ? "dayLabel mono axis axis-workweek" : "dayLabel mono axis"); });
+ // x axis Dates
 
-var timeLabels = svg.selectAll(".timeLabel")
-.data(times)
-.enter().append("text")
-  .text(function(d) { return d; })
-  .attr("x", function(d, i) { return i * gridSize; })
-  .attr("y", 0)
-  .style("text-anchor", "middle")
-  .attr("transform", "translate(" + gridSize / 2 + ", -6)")
-  .attr("class", function(d, i) { return ((i >= 7 && i <= 16) ? "timeLabel mono axis axis-worktime" : "timeLabel mono axis"); });
+d3.csv(data.csv, function(data) {
+    var dateLabels = svg.selectAll(".DateLabel")
+        .data(data, function(d){ return d.Date})
+        .enter()
+        .append("text")
+        .text(function(d) {return d;})
+        .attr("x", function(d, i) { return i * gridSize; })
+        .attr("y", 0)
+        .style("text-anchor", "middle")
+        .attr("transform", "translate(" + gridSize / 2 + ", -6)")
+        .attr("class", function(d, i) { return ((i >= 7 && i <= 16) ? "timeLabel mono axis axis-worktime" : "timeLabel mono axis"); });
+});
+// y axis Factors
+// var dayLabels = svg.selectAll(".dayLabel")
+// .data(days)
+// .enter().append("text")
+//   .text(function (d) { return d; })
+//   .attr("x", 0)
+//   .attr("y", function (d, i) { return i * gridSize; })
+//   .style("text-anchor", "end")
+//   .attr("transform", "translate(-6," + gridSize / 1.5 + ")")
+//   .attr("class", function (d, i) { return ((i >= 0 && i <= 4) ? "dayLabel mono axis axis-workweek" : "dayLabel mono axis"); });
 
+
+/*
 var heatmapChart = function(tsvFile) {
 d3.tsv(tsvFile,
 function(d) {
@@ -108,3 +119,4 @@ datasetpicker.enter()
 .on("click", function(d) {
 heatmapChart(d);
 });
+*/
