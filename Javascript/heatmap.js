fe1c122de68d7fbab98e5b6b5916ac8ab4ac708b
Teresa Ting <7157708+mochabits@users.noreply.github.com>
2019-01-07 17:50:04 -0700

    Add files via upload

--- Javascript/heatmap.js
+++ Javascript/heatmap.js
@@ -1,83 +1,152 @@
-// chart settings
-var margin = { top: 50, right: 0, bottom: 100, left: 75 },
-width = 700 - margin.left - margin.right,
-height = 600 - margin.top - margin.bottom,
-gridSize = Math.floor(width / 20),
-legendElementWidth = gridSize*2,
-buckets = 9,
-colors = ["#ffffd9","#edf8b1","#c7e9b4","#7fcdbb","#41b6c4","#1d91c0","#225ea8","#253494","#081d58"]; // alternatively colorbrewer.YlGnBu[9]
+/* 
+ * All D3 visuals run in a frame with the following elements/variables:
+ * 
+ * SVG element: 
+ * - <svg xmlns="http://www.w3.org/2000/svg" class="chart" id="chart" >
+ * 
+ * pbi object:
+ * - 'dsv'    : function that retrieves the data via the provided callback: pbi.dsv(callback)
+                e.g. pbi.dsv(function(data) { //Process data function });
+ * - 'height' : height of the sandbox frame
+ * - 'width'  : width of the sandbox frame
+ * - 'colors' : color array with 8 colors; changable via options
+ * 
+ * Code is based on: http://bl.ocks.org/tjdecke/5558084
+ * Remember to comment / uncomment var pbi and pbi.dsv(function(data) { 
+ */
 
-var svg = d3.select("#chart").append("svg")
-.attr("width", width + margin.left + margin.right)
-.attr("height", height + margin.top + margin.bottom)
-.append("g")
-.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+//var pbi = {width:500,height:270};
+var margin = { top: 20, right: 70, bottom: 60, left: 30 },
+    width = pbi.width - margin.left - margin.right,   
+    height = pbi.height - margin.top - margin.bottom, 
+    xGridSize = Math.floor(width / 4.2),
+    yGridSize = Math.floor(height / 3.3),
+    colors = ["#336699", "#ffffcc", "#b33c00"]; // min, middle, max values
 
-d3.csv("data.csv", function(data) {
-    //console.log(data);
-    var factors = [];
-    var dates = [];
+var x = d3.scale.linear().range([0, width]),
+    y = d3.scale.linear().range([0, height]);
+
+// Text Color object. It has to be placed before the callback function is called
+var textColor = {
+        hex_to_rgb: function(hex) {
+            var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);         	
+            return result ? { 
+                    r: parseInt(result[1], 16),
+                    g: parseInt(result[2], 16),
+                    b: parseInt(result[3], 16) 
+            } : null;
+        },
+        hex_inverse_bw: function(hex) {
+            var rgb = textColor.hex_to_rgb(hex);
+            var luminance = (0.2126*rgb["r"] + 0.7152*rgb["g"] + 0.0722*rgb["b"]);
+            return (luminance < 140) ? "#ffffff": "#000000";
+        }
+    };
+
+// Canvas
+var svg = d3.select("#chart") // default SVG ID
+    .attr("width", pbi.width )   
+    .attr("height", pbi.height )
+   
+//d3.csv("data.csv", function(data) {
+pbi.dsv(function(data) {
+
+    // build arrays for x and y axes
+    var factors = []; // y axis
+    var dates = []; // x axis
 
     for (var i = 0; i < data.length; i++) {
-        if (factors.indexOf(data[i].Factors) === -1) {
-            factors.push(data[i].Factors)
+        if (factors.indexOf(data[i].factor) === -1) {
+            factors.push(data[i].factor);
+          
         }
-        if (dates.indexOf(data[i].Date) === -1) {
-            dates.push(data[i].Date);
+        if (dates.indexOf(data[i].date) === -1) {
+            dates.push(data[i].date);
         }
     }
-    //console.log(dates);   
-    // x axis Dates
-    var dateLabels = svg.selectAll(".DateLabel")
-        .data(data, function(d){ return d.Date;})
+  
+    factors.sort();
+    dates.sort(function(a,b) {
+      return new Date(a) - new Date(b);
+    });
+    console.log(factors);
+    console.log(dates);
+
+  /*
+ 	// X Axis
+	var dateLabels = 
+        svg.selectAll(".dateLabel")
+        //.data(days)
+  		.data(data, function(d){return d.date;})
         .enter()
-        .append("text")
-        .text(function(d) {return d.Date;})
-        .attr("x", function(d, i) { return i * gridSize; })
-        .attr("y", 0)
+      	.append("text")
+        .text(function (d) { return d.date; })
+        .attr("x", function (d, i) { return (i+1) * xGridSize; })
+        .attr("y", 0) 		
         .style("text-anchor", "middle")
-        .attr("transform", "translate(" + gridSize / 2 + ", -6)")
-        //.attr("class", function(d, i) { return ((i >= 7 && i <= 16) ? "timeLabel mono axis axis-worktime" : "timeLabel mono axis"); })
-        ;
-    
-    // y axis Factors
+        .attr("transform", "translate(-6," + xGridSize / 1.5 + ")");
+	// Y axis
     var factorLabels = svg.selectAll(".factorLabel")
-    .data(data, function(d){ return d.Factors;})
-    .enter().append("text")
-    .text(function (d) { return d.Factors; })
-    .attr("x", 0)
-    .attr("y", function (d, i) { return i * gridSize; })
-    .style("text-anchor", "end")
-    .attr("transform", "translate(-6," + gridSize / 1.5 + ")")
-    //.attr("class", function (d, i) { return ((i >= 0 && i <= 4) ? "dayLabel mono axis axis-workweek" : "dayLabel mono axis"); })
-    ;
+        .data(data, function(d){return d.factor;})
+        .enter().append("text")
+        .text(function (d) { return d.factor; })
+        .attr("x", 0)
+        .attr("y", function (d, i) { return (i) * yGridSize + margin.top + yPlaceHolder; })
+        .style("text-anchor", "end")
+        .attr("transform", "translate(-6," + yGridSize / 1.5 + ")");
+*/	
+  	// Color scale
+    var colorScale = d3.scale
+    .linear()
+    .domain([d3.min(data, function (d) { return  parseInt(d.value); }), 
+            0, 
+            d3.max(data, function (d) { return parseInt(d.value); })])
+    .range(colors)
+    .interpolate(d3.interpolateHcl);
+ 
+    console.log("min and max value");
+    console.log(d3.extent(data, function (d) { return parseInt(d.value); }));
 
-    var colorScale = d3.scale.quantile()
-    .domain([0, buckets - 1, d3.max(data, function (d) { return d.value; })])
-    .range(colors);
-
-    // the heatmap 
     var cards = svg.selectAll(".Value")
-        .data(data, function(d) {return d.Date+':'+d.Factors;});
-
+        .data(data, function(d) {return d.date+':'+d.factor;});
+    
+  	// Calculate the domains based on the data:
+    x.domain(d3.extent(data, function(d) { return dates.indexOf(d.date); }));
+    y.domain(d3.extent(data, function(d) { return factors.indexOf(d.factor); }));
+ 
+    // Heatmap
     cards.append("title");
     cards.enter().append("rect")
-    //cards.append("rect")
         .attr("x", function(d,i) { 
-            return (dates.indexOf(d.Date) * gridSize); 
+            return x(dates.indexOf(d.date)); 
         })
         .attr("y", function(d,i) { 
-            return (factors.indexOf(d.Factors)*gridSize); 
+            return y(factors.indexOf(d.factor)); 
          })
-        .attr("rx", 4)
-        .attr("ry", 4)
-        .attr("class", "hour bordered")
-        .attr("width", gridSize)
-        .attr("height", gridSize)
-        .style("fill", colors[0]);
-
+        .attr("width", xGridSize)
+        .attr("height",yGridSize)
+        .style("fill", colors[1]);
+ 
     cards.transition().duration(1000)
-         .style("fill", function(d) { return colorScale(d.Value); });
-    cards.select("title").text(function(d) { return d.Value; });
-    cards.exit().remove();   
+         .style("fill", function(d) { return colorScale(d.value); });
+    cards.select("title").text(function(d) { 
+        return d.value; 
     });
+    cards.exit().remove();   
+    
+    // insert value for each card
+    cards.enter().append("text")
+     .attr("x", function(d,i) { 
+        return x(dates.indexOf(d.date))+xGridSize/2 - 13; 
+    })
+    .attr("y", function(d,i) { 
+        return y(factors.indexOf(d.factor))+yGridSize/2; 
+     })
+    .attr("dominant-baseline","middle")
+    .attr("class","valueLabel")
+    .style("fill",function(d) { 
+  					return textColor.hex_inverse_bw(colorScale(d.value)); 
+    	})	
+  	
+  	.text(function(d) { return d.value;  });
+});
--- Javascript/heatmap_pbi.js
+++ Javascript/heatmap_pbi.js
@@ -12,16 +12,18 @@
  * - 'colors' : color array with 8 colors; changable via options
  * 
  * Code is based on: http://bl.ocks.org/tjdecke/5558084
+ * Remember to comment / uncomment var pbi and pbi.dsv(function(data) { 
  */
+
 var pbi = {width:500,height:270};
-var margin = { top: 20, right: 60, bottom: 60, left: 30 },
+var margin = { top: 20, right: 70, bottom: 60, left: 30 },
     width = pbi.width - margin.left - margin.right,   
     height = pbi.height - margin.top - margin.bottom, 
     xGridSize = Math.floor(width / 4.2),
     yGridSize = Math.floor(height / 3.3),
-    buckets = 9,
-    colors = ["#67a9cf", "#f7f7f7", "#ef8a62"]; 
-
+    //colors = ["#336699", "#ffffcc", "#cc3300"]; // min, middle, max values
+    colors = ["#663300", "#ffffff", "#993300"]; // min, middle, max values
+//"#67A9CF", "#F7F7F7", "#EF8A62"
 var x = d3.scale.linear().range([0, width]),
     y = d3.scale.linear().range([0, height]);
 
@@ -29,15 +31,13 @@ var x = d3.scale.linear().range([0, width]),
 var svg = d3.select("#chart") // default SVG ID
     .attr("width", pbi.width )   
     .attr("height", pbi.height )
-    //.append("g")
-    //.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
    
 d3.csv("data.csv", function(data) {
 //pbi.dsv(function(data) {
 
     // build arrays for x and y axes
-    var factors = [];
-    var dates = [];
+    var factors = []; // y axis
+    var dates = []; // x axis
 
     for (var i = 0; i < data.length; i++) {
         if (factors.indexOf(data[i].factor) === -1) {
@@ -49,9 +49,13 @@ d3.csv("data.csv", function(data) {
         }
     }
     factors.sort();
-    dates.sort();
+    dates.sort(function(a,b) {
+      return new Date(a) - new Date(b);
+    });
     console.log(factors);
     console.log(dates);
+
+
   /*
  	// X Axis
 	var dateLabels = 
@@ -65,7 +69,6 @@ d3.csv("data.csv", function(data) {
         .attr("y", 0) 		
         .style("text-anchor", "middle")
         .attr("transform", "translate(-6," + xGridSize / 1.5 + ")");
-
 	// Y axis
     var factorLabels = svg.selectAll(".factorLabel")
         .data(data, function(d){return d.factor;})
@@ -85,8 +88,8 @@ d3.csv("data.csv", function(data) {
     .range(colors)
     .interpolate(d3.interpolateHcl);
  
-    //console.log("min and max value");
-    //console.log(d3.extent(data, function (d) { return parseInt(d.value); }));
+    console.log("min and max value");
+    console.log(d3.extent(data, function (d) { return parseInt(d.value); }));
 
     var cards = svg.selectAll(".Value")
         .data(data, function(d) {return d.date+':'+d.factor;});
@@ -108,7 +111,7 @@ d3.csv("data.csv", function(data) {
          })
         .attr("width", xGridSize)
         .attr("height",yGridSize)
-        .style("fill", colors[0]);
+        .style("fill", colors[1]);
  
     cards.transition().duration(1000)
          .style("fill", function(d) { return colorScale(d.value); });
@@ -117,6 +120,60 @@ d3.csv("data.csv", function(data) {
     });
     cards.exit().remove();   
     
+    // decide text color version 1
+    /*
+    var textColor = {
+        hex_to_rgb: function(hex) {
+            var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+            return result ? { 
+                    r: parseInt(result[1], 16),
+                    g: parseInt(result[2], 16),
+                    b: parseInt(result[3], 16) 
+            } : null;
+        },
+        hex_inverse_bw: function(hex) {
+            rgb = this.hex_to_rgb(hex);
+            luminance = (0.2126*rgb["r"] + 0.7152*rgb["g"] + 0.0722*rgb["b"]);
+            return (luminance < 140) ? "#ffffff": "#000000";
+        }
+    };
+    */
+   // version 2
+    var padZero = function (str, len) {
+        len = len || 2;
+        var zeros = new Array(len).join('0');
+        return (zeros + str).slice(-len);
+    };
+
+    var invertColor = function (hex) {
+        bw = true;
+        if (hex.indexOf('#') === 0) {
+            hex = hex.slice(1);
+        }
+        // convert 3-digit hex to 6-digits.
+        if (hex.length === 3) {
+            hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
+        }
+        if (hex.length !== 6) {
+            throw new Error('Invalid HEX color.');
+        }
+        var r = parseInt(hex.slice(0, 2), 16),
+            g = parseInt(hex.slice(2, 4), 16),
+            b = parseInt(hex.slice(4, 6), 16);
+        if (bw) {
+            // http://stackoverflow.com/a/3943023/112731
+            return (r * 0.299 + g * 0.587 + b * 0.114) > 186
+                ? '#000000'
+                : '#FFFFFF';
+        }
+        // invert color components
+        r = (255 - r).toString(16);
+        g = (255 - g).toString(16);
+        b = (255 - b).toString(16);
+        // pad each with zeros and return
+        return "#" + padZero(r) + padZero(g) + padZero(b);
+    }
+
     // insert value for each card
     cards.enter().append("text")
      .attr("x", function(d,i) { 
@@ -125,8 +182,10 @@ d3.csv("data.csv", function(data) {
     .attr("y", function(d,i) { 
         return y(factors.indexOf(d.factor))+yGridSize/2; 
      })
+    .attr("dominant-baseline","middle")
     .attr("class","valueLabel")
-    //.attr("dy", ".35em")
+    .style("fill",function(d) { 
+        return invertColor(colorScale(d.value)); 
+})
     .text(function(d) { return d.value; });          
     });
-   
