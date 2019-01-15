71c7eb88373fe2201e841525b6c9179ead782d32
teresating886 <teresating886@gmail.com>
2019-01-14 20:01:34 -0700

    Update L22.js

--- Javascript/L22.js
+++ Javascript/L22.js
@@ -1,66 +1,69 @@
-// select svg container
-const svg = d3.select(".canvas")
-              .append("svg")
-              .attr("width",600)
-              .attr("height",600);
+// select the svg container first
+const svg = d3.select('.canvas')
+  .append('svg')
+    .attr('width', 600)
+    .attr('height', 600);
 
-// create margins and dimensions
+// create margins & dimensions
 const margin = {top: 20, right: 20, bottom: 100, left: 100};
-const graphWidth = 600 - margin.right - margin.left;
+const graphWidth = 600 - margin.left - margin.right;
 const graphHeight = 600 - margin.top - margin.bottom;
 
-const graph = svg.append("g")
-                 .attr("width", graphWidth)
-                 .attr("height", graphHeight)
-                 .attr("transform",`translate(${margin.left},${margin.top})`)
+const graph = svg.append('g')
+  .attr('width', graphWidth)
+  .attr('height', graphHeight)
+  .attr('transform', `translate(${margin.left}, ${margin.top})`);
 
-// create axis
-const xAxisGroup = graph.append("g")
-      .attr("transform",`translate(0,${graphHeight})`);
-const yAxisGroup = graph.append("g");
+// create axes groups
+const xAxisGroup = graph.append('g')
+  .attr('transform', `translate(0, ${graphHeight})`)
 
+const yAxisGroup = graph.append('g');
 
-d3.json("menu.json").then(data => {
-  const min = d3.min(data,d => d.orders);
-  const max = d3.max(data,d => d.orders);
-  const extent = d3.extent(data, d => d.orders);
-const y = d3.scaleLinear()
-            .domain([0,extent[1]])
-            .range([graphHeight,0]);
+d3.json('menu.json').then(data => {
 
+  const y = d3.scaleLinear()
+    .domain([0, d3.max(data, d => d.orders)])
+    .range([graphHeight, 0]);
+p
+  const x = d3.scaleBand()
+    .domain(data.map(item => item.name))
+    .range([0, graphWidth])
+    .paddingInner(0.2)
+    .paddingOuter(0.2);
 
-const x = d3.scaleBand()
-          .domain(data.map(item => item.name) )
-          .range([0,500])
-          .paddingInner(0.2)
-          .paddingOuter(0.5);
+  // join the data to circs
+  const rects = graph.selectAll('rect')
+    .data(data);
 
-// testing if it works
-// console.log(y(400));
-// console.log(y(0));
-// console.log(y(800));
+  // add attrs to circs already in the DOM
+  rects.attr('width', x.bandwidth)
+    .attr("height", d => graphHeight - y(d.orders))
+    .attr('fill', 'orange')
+    .attr('x', d => x(d.name))
+    .attr('y', d => y(d.orders));
 
-  // join the data to rects
-  const rects = graph.selectAll("rect")
-                   .data(data)
-        rects.attr("width", x.bandwidth)
-             .attr("height",d => graphHeight - y(d.orders) )
-             .attr("fill","orange")
-             .attr("x", d => x(d.name))
-             .attr("y", d=> y(d.orders));
-  // append the rest of rects
+  // append the enter selection to the DOM
   rects.enter()
-       .append("rect")
-       .attr("width", x.bandwidth)
-      .attr("height",d => graphHeight - y(d.orders) )
-      .attr("fill","orange")
-      .attr("x", d => x(d.name))
-      .attr("y", d=> y(d.orders));
-  
-  // create and call the axes
+    .append('rect')
+      .attr('width', x.bandwidth)
+      .attr("height", d => graphHeight - y(d.orders))
+      .attr('fill', 'orange')
+      .attr('x', (d) => x(d.name))
+      .attr('y', d => y(d.orders));
+
+  // create & call axesit
   const xAxis = d3.axisBottom(x);
-  const yAxis = d3.axisLeft(y);
+  const yAxis = d3.axisLeft(y)
+    .ticks(3)
+    .tickFormat(d => d + ' orders');
 
   xAxisGroup.call(xAxis);
   yAxisGroup.call(yAxis);
-})
+
+  xAxisGroup.selectAll('text')
+    .attr('fill', 'orange')
+    .attr('transform', 'rotate(-40)')
+    .attr('text-anchor', 'end')
+
+});
